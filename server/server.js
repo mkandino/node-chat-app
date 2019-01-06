2658806e407bef541eb76256888bd6f9c0b2f4e7
mkandino <bmikekandino@gmail.com>
2019-01-06 17:39:37 -0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    io.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        created: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: 'New user joined',
+        created: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log("createMessage", message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            created: new Date().getTime()
-        });
+            createdAt: new Date().getTime()
+        })
     });
 
 
