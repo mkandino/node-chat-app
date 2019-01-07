cd1a46fd5f63b7d345c0ffe872c8a396ca62a2f3
mkandino <bmikekandino@gmail.com>
2019-01-06 20:24:57 -0500

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 var socket = io();
 var $ = jQuery;
+// var mustache = require('mustache');
 
 socket.on('connect', function () {
     console.log("Connected to server");
@@ -9,25 +10,44 @@ socket.on('disconnect', function () {
     console.log("Disconnected from server");
 });
 
-socket.on('newMessage', (message) => {
-    console.log("newMessage", message);
+socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format("h:mm a");
 
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
+
+    // console.log("newMessage", message);
+
+    // var li = $('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // $('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format("h:mm a");
-    var li = $('<li></li>');
-    var a = $("<a target='_blank'>My current location</a>");
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    $('#messages').append(html);
+    
+    // var li = $('<li></li>');
+    // var a = $("<a target='_blank'>My current location</a>");
+
+    // li.text(`${message.from} ${formattedTime}:`);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // $('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
